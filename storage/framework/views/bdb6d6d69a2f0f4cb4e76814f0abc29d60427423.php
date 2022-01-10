c7ea66a30d28a7fe590ae5349defb94dc6a20b33
Syanda Faturochman <93498649+syanda54@users.noreply.github.com>
2022-01-10 15:34:40 +0700

    Add files via upload

---
+++ storage/framework/views/bdb6d6d69a2f0f4cb4e76814f0abc29d60427423.php
@@ -0,0 +1,31 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://www.mobilinanews.com/cdn/posts/1/2020/2020-02-07/30eec12cf0106fd98bb51a64c4bcc728_1.jpg">
+            <div class="card-body">
+              <h5 class="card-title">SIC dan SIKMC</h5>
+              <p class="card-text"> Sentul International Circuit (SIC) merupakan satu-satunya sirkuit permanen yang juga bisa digunakan untuk balap mobil. Pun dengan motor supersport dan superbike yang sering menghelat latihan dan event di sirkuit sepanjang 3,9 km ini. Kalau untuk versi kecilnya, ada Sentul International Karting and Motorcycle Circuit (SIKMC) yang memang diperuntukan untuk motor bebek atau gokart. Dengan panjang 1,2 km, sirkuit yang biasa disebut 'Sentul Kecil' ini sering digunakan untuk latihan motor bebek, juga sport 150 cc.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://otomotifzone.com/wp-content/uploads/2018/09/INDOCLUB3.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Circuit Gery Mang Subang</h5>
+              <p class="card-text"> Kalau ada yang bosan latihan di Sentul Kecil, biasanya sirkuit Gery Mang ini bisa jadi alternatif lokasi latihan. </p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\blog-Circuit\resources\views/index.blade.php ENDPATH**/ ?>
