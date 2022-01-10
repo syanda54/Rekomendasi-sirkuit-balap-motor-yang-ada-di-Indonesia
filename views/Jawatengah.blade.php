e72fa9d5deae8af1c2463141619bc2862f5c6993
Syanda Faturochman <93498649+syanda54@users.noreply.github.com>
2022-01-10 15:33:31 +0700

    Add files via upload

---
+++ views/Jawatengah.blade.php
@@ -0,0 +1,30 @@
+@extends('master')
+@section('content')
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://images.solopos.com/2021/09/sirkuit-boyolali-.jpg">
+            <div class="card-body">
+              <h5 class="card-title">Circuit Bonyolali</h5>
+              <p class="card-text"> Setelah rampung pada bagian lintasan, sirkuit di Boyolali, Jateng ini langsung banyak digunakan untuk club event dan fun race lokal. Awalnya, sirkuit sepanjang 1,3 km ini memang lebih difokuskan untuk kompetisi gokart.</p>
+            
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+      <div class="col-sm">
+        <div class="card" style="width: 50rem;">
+            <img src="https://cdn.idntimes.com/content-images/post/20190118/sirkuit-gelora-bung-tomo-9a723dc693c2c2712a8271950a6bf384_600x400.png">
+            <div class="card-body">
+              <h5 class="card-title">Circuit Gelora Bung Tomo</h5>
+              <p class="card-text"> Pada tahun 2015, Tri Rismaharini, Walikota Surabaya, Jatim memang sudah mengumumkan akan membangun sirkuit di Surabaya, tepatnya di daerah Benowo. Tujuannya jelas, ia mulai resah dengan balap liar di Surabaya dan sekitarnya. Makanya bersamaan dengan pembangunan Gelora Bung Tomo (GBT) sebagai markas dari tim sepakbola, Persebaya FC dibangun juga sirkuit balap motor. Makanya sekarang GBT juga sebagai sport center dengan beragam fasilitas olahraga.</p>
+</div>            
+</div>
+</div>
+</div>
+</div>           
+@endsection
